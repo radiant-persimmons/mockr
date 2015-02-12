6e964652c7aa8a18d0c0616a438526020bca7909
Ruben V <ruben1586@gmail.com>
2015-02-12 15:14:38 -0800

    fix function

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,7 +2,7 @@ function() {
 
 var router = require('express').Router();
 
-module.exports = applicationRouter(app) {
+module.exports = function applicationRouter(app) {
 	
   require('./api/user')(router);
   require('./api/endpoint')(router);
