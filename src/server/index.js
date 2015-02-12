dfdbef83c074125078f2c3348c2c1abeaee19666
Ruben V <ruben1586@gmail.com>
2015-02-12 11:50:43 -0800

    env variables

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,14 @@
-(function() {
-  'use strict';
+'use strict';
 
-  var express  = require('express');
-  var app = express();
+var express  = require('express');
+var app = express();
 
-  require('./env')(app);
-  require('./routes')(app);
+require('./env')(app);
+require('./routes')(app);
 
-  app.listen(app.get('port'), function() {
-    console.log('listening on port ', app.get('port'));
-  });
+app.listen(app.get('port'), function() {
+  console.log('listening on port ', app.get('port'));
+});
 
-  module.exports = app;
+module.exports = app;
 
-})();
