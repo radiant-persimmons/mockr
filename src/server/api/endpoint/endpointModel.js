000682687832061c121f459093a58beffefd245a
Ruben V <ruben1586@gmail.com>
2015-02-12 09:23:53 -0800

    add db skeleton

--- src/server/api/endpoint/endpointModel.js
+++ src/server/api/endpoint/endpointModel.js
@@ -1,2 +1,9 @@
 var mongoose = require('mongoose');
 var db = require('../db');
+
+var endpointSchema = mongoose.Schema({
+});
+
+var Endpoint = mongoose.model('Endpoint', endpointSchema);
+
+module.exports = Endpoint;
