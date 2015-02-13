3b1a2a5932e279bf06b6213afd09251f87fe1625
Ruben V <ruben1586@gmail.com>
2015-02-13 10:27:16 -0800

    refactor passport

--- src/server/api/auth/index.js
+++ src/server/api/auth/index.js
@@ -7,17 +7,8 @@ module.exports = function(router) {
   router.route('/api/auth/login')
     .post(AuthController.login)
 
-  router.get('/auth/github',
-  passport.authenticate('github'),
-  function(req, res){
-    // The request will be redirected to GitHub for authentication, so this
-    // function will not be called.
-  });
+  router.get('/auth/github', AuthController.authentication);
 
-  app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+  app.get('/auth/github/callback', AuthController.authenticationCallback);
 
 };
