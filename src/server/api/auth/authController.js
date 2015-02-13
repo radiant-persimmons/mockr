3d1ce37db4614d356a358490f4772879755149f7
Ruben V <ruben1586@gmail.com>
2015-02-13 14:39:42 -0800

    fix lint test

--- src/server/api/auth/authController.js
+++ src/server/api/auth/authController.js
@@ -12,18 +12,18 @@ var authentication = function(req, res, next) {
   function(req, res){
     // The request will be redirected to GitHub for authentication, so this
     // function will not be called.
-  }
+  };
 };
 
 var authenticationCallback = function(req, res, next) {
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
-  }
+  };
 };
 
 module.exports = {
   login: login,
   authentication: authentication,
   authenticationCallback: authenticationCallback
-}
+};
