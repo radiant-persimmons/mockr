b7e8d1b8a15b363ecbf656de21de4b303177743f
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-02-13 17:07:00 -0800

    add ui-router

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,22 +1,50 @@
 ;(function(){
-  angular.module('app', [
-    'ngRoute',
-    'mockr-login',
-    'mockr-dashboard'
+
+  function config($stateProvider, $locationProvider, $urlRouterProvider) {
+
+    $stateProvider
+      /**
+       * @name landing
+       * @type {route}
+       * @description First page for incoming users, and for default routing
+       *              for all failed routes.
+       */
+      .state('landing', {
+        url: '/',
+        templateUrl: '/core/views/home.html',
+        controller: 'LandingController',
+        controllerAs: 'LandingCtrl'
+      })
+
+      /**
+       * @name dashboard
+       * @type {route}
+       * @description User landing page, the main display.
+       */
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: '/html/core/views/dashboard.html',
+        controller: 'DashBoardController',
+        controllerAs: 'DashBoardCtrl'
+      })
+
+    // default uncaught routes to landing page
+    $urlRouterProvider.otherwise('/');
+
+    // enable HTML5 mode
+    $locationProvider.html5Mode(true);
+  }
+
+  config.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+
+
+
+  angular
+    .module('app', [
+      'mockr-login',
+      'mockr-dashboard',
+      'ui.router'
     ])
-  .config(function($routeProvider, $httpProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: './core/views/home.jade',
-        controller: 'LoginController'
-      })
-      .when('/login', {
-        controller: 'LoginController'
-      })
-      .when('/dashboard',{
-        templateUrl: 'html/core/views/dashboard.html',
-        controller: 'DashBoardController'
-      });
-    $locationProvider.html5Mode = true;
-  });
+    .config(config);
+
 })();
