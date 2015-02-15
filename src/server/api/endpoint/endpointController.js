3f7d1c4e793f24164a5a582741780305879270f6
Ruben V <ruben1586@gmail.com>
2015-02-14 17:21:52 -0800

    update endpoint controller

--- src/server/api/endpoint/endpointController.js
+++ src/server/api/endpoint/endpointController.js
@@ -2,8 +2,8 @@ var Endpoint = require('./endpointModel');
 var User = require('../user/userModel');
 
 var getEndpoints = function(req, res, next) {
-  var username = req.params.user;
-    User.findOne({'username': user}, function (err, user) {
+  var username = req.params.username;
+    User.findOne({'username': username}, function (err, user) {
       if (err) return res.end(err);
       //return user
       console.log('endpoints -->', user.endpoints);
@@ -12,14 +12,27 @@ var getEndpoints = function(req, res, next) {
 };
 
 var createEndpoint = function(req, res, next) {
-  var user = req.params.user;
+  var username = req.params.username;
   var path = req.body.path;
   var verb = req.body.verb;
   var data = req.body.data;
+  var obj = {verb: verb, data: data};
+  var container = {};
+  container[path] = obj
 
-  User.findOne({'username': user}, function (err, user) {
+  User.findOne({'username': username}, function (err, user) {
     if (err) return res.end(err);
-    
+    if(!user) {
+      res.status(500).end();
+    } else {
+      User.update({username: username}, { 
+      	$set: { endpoints: container } 
+      }, function(err, numAffected, rawResponse) {
+        if (err) return res.status(500).json({ message: err });
+          console.log(rawResponse);
+          res.status(201).json(rawResponse);
+      });
+    }
   });
 };
 
--- src/server/api/endpoint/index.js
+++ src/server/api/endpoint/index.js
@@ -1,11 +1,11 @@
 var Endpoint = require('./endpointController.js');
 
 module.exports = function(router) {
-  router.route('/api/users/:user/endpoints')
+  router.route('/api/users/:username/endpoints')
     .get(Endpoint.getEndpoints)
     .post(Endpoint.createEndpoint);
 
-  router.route('/api/users/:user/endpoints/:id')
+  router.route('/api/users/:username/endpoints/:id')
     .get(Endpoint.getEndpoint)
     .put(Endpoint.editEndpoint)
     .delete();
