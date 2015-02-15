187a34e48ea923582770075278f0657e247b8911
Ruben V <ruben1586@gmail.com>
2015-02-14 16:20:19 -0800

    change utils function

--- src/server/utils/index.js
+++ src/server/utils/index.js
@@ -1,14 +1,20 @@
 var User = require('../api/user/userModel');
 
 var createUserIfNotExistant = function(user, cb) {
-  var newUser = new User({ 'username': user.username, 'userID': user.id });
-  newUser.save(function (err) {
-    if (err){
-      console.log('Error: ', err);
-      return cb(err, null);
-    } 
-    console.log('User created');
-    cb(null, true);
+  User.findOne({'username': user.username}, function(err, user) {
+    if(!user) {
+      var newUser = new User({ 'username': user.username, 'userID': user.id });
+      newUser.save(function (err, user) {
+        if (err){
+          console.log('Error: ', err);
+          return cb(err, null);
+        } 
+        console.log('User created');
+        return cb(null, user);
+      });
+    } else {
+      return cb(null, user);
+    }
   });
 };
 
