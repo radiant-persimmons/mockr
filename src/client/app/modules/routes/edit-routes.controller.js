661f29ce891ac079562ea2e25706f388abd5ac72
Ruben V <ruben1586@gmail.com>
2015-02-18 12:11:22 -0800

    add get route function on factory

--- src/client/app/modules/routes/edit-routes.controller.js
+++ src/client/app/modules/routes/edit-routes.controller.js
@@ -42,6 +42,7 @@
       };
       vm.formInfo.methods = Object.keys(vm.formInfo.body);
 
+<<<<<<< HEAD
       // // get route info from server TODO
       // routes.getRoute(vm.formInfo.route)
       //   .then(function(res) {
@@ -50,6 +51,10 @@
       //   }).catch(function(err) {
       //     console.error('error fetching route', vm.formInfo.route);
       //   });
+=======
+      // // get route info from server
+      getRoute();
+>>>>>>> add get route function on factory
     }
 
     /**
@@ -98,6 +103,16 @@
 
       // update keys
       vm.formInfo.methods = Object.keys(vm.formInfo.body);
+    }
+
+    function getRoute() {
+      RouteFactory.getRoute($stateParams.route)
+        .then(function(res) {
+          console.log('route has been fetched');
+
+        }).catch(function(err) {
+          console.error('error fetching route', vm.formInfo.route);
+        });
     }
   }
 
---
+++ src/client/app/modules/routes/routes.service.js
@@ -0,0 +1,76 @@
+;(function(){
+
+  angular
+    .module('app.services.routes', ['app.services.user'])
+    .factory('routes', routes);
+
+  routes.$inject = ['$http', 'user'];
+
+  function routes ($http, user) {
+    var _this = this;
+    _this.routes = [];
+
+    this.deleteRoute = function (){};
+
+    this.addRoute = function(body){
+
+      var route = {};
+      route.method = 'GET';
+      route.route = body;
+      route.responseStatus = 200;
+      this.routes.push(route);
+      return $http({
+        method: 'POST',
+        url: '/api/users/' + user.username + '/endpoints',
+        data: route
+      }).success(function(result) {
+        console.log('ADD ROUTE SUCCESS:', result);
+      }).error(function(err) {
+        console.log('ADD ROUTE ERROR:', err);
+      });
+
+    };
+
+    this.updateRoute = function(body){
+      return $http({
+        method: 'PUT',
+        url: '/api/users/' + user.username + '/endpoints',
+        data: body
+      }).success(function(result) {
+        console.log('UPDATE SUCCESS:', result);
+      }).error(function(err) {
+        console.log('UPDATE ERROR:', err);
+      });
+    };
+
+    this.fetch = function(user) {
+      return $http({
+        method: 'GET',
+        url: '/api/users/' + user.username + '/endpoints',
+      }).success(function(result) {
+        for (var route in result) {
+          _this.routes.push(result[route]);
+        }
+      }).error(function(err) {
+        console.log('ERROR!!', err);
+      });
+    };
+
+    this.getRoute = function(route) {
+      return $http({
+        method: 'GET',
+        url: '/api/users/' + UserFactory.username + '/endpoints' + route
+      }).success(function(result) {
+        console.log('result', result);
+        //for (var route in result) {
+          //_this.routes.push(result[route]);
+        //}
+      }).error(function(err) {
+        console.log('ERROR!!', err);
+      });
+    };
+
+    return this;
+  }
+
+})();
