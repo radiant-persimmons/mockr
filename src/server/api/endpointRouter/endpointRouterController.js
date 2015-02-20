5be2234cb350e0806e695bf5f9032edda0613f24
Ruben V <ruben1586@gmail.com>
2015-02-19 20:50:25 -0800

    add editData to endpointRouter, have to test it

--- src/server/api/endpointRouter/endpointRouterController.js
+++ src/server/api/endpointRouter/endpointRouterController.js
@@ -78,7 +78,50 @@ var postData = function(req, res, next) {
 };
 
 var changeData = function(req, res, next) {
+  var username = req.params.username;
+  var route = req.params[0];
+  var method = req.method;
 
+  Endpoint.findOne({ 'username': username, 'route': route }, function (err, endpoint) {
+    if (err) return res.status(500).end(err);
+    if(!endpoint) {
+      return res.status(500).end(err);
+    } else {
+
+      var statusCode = endpoint.methods[method].status;
+      
+      //if persistance is set to true, we let the user persist data through their API endpoint
+      if(endpoint.persistance === true) {
+        var params = req.body;
+        //we need a parameter passed to know what to change
+        if(!req.query.id) {
+          //we need some data to know what to look for
+          return res.status(500).end();
+        } else {
+          var query = parseInt(req.query.id);
+          for(var i=0; i<endpoint.data.length; i++) {
+            var dataPoint = endpoint.data[i];
+            if(dataPoint.id === query) {
+              //update data
+              delete dataPoint;
+              Endpoint.update({ 'username': username, 'route': route }, {$set: {'data': params}}, function(err, numAffected, rawResponse) {
+                if (err) return res.status(500).json(err); 
+                console.log(numAffected, rawResponse);
+                res.status(statusCode).end();
+                }); 
+              return res.status(statusCode).json();
+            }
+          }
+          return res.status(500).end();
+        }
+      } else {
+        //get data from user input
+        //check what verb is normally used?? PUT or PATCH
+        var data = endpoint.methods[method].data;
+        res.status(statusCode).json(data);
+      }
+    }
+  });
 };
 
 var deleteData = function(req, res, next) {
