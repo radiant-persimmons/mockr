289ad874e5b4c0e2a8b15df3a9ae5c6abfcfaacf
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-02-24 14:22:32 -0800

    fix pr comments

--- src/server/utils/index.js
+++ src/server/utils/index.js
@@ -4,36 +4,10 @@ var createUserIfNotExistant = function(user, cb) {
   User.findOneAndUpdate({ username: user.username }, user,
                         { upsert: true }, function(err, numAffected, raw) {
     if (err) return cb(err, null);
-    console.log('numAffected', numAffected);
-    console.log('raw response', raw);
-    console.log('new user created', user);
     return cb(null, user);
   });
-  // User.findOne({'username': user.username}, function(err, existingUser) {
-  //   if(!existingUser) {
-  //     var newUser = new User({ 'username': user.username, 'userID': user.id });
-  //     newUser.save(function (err, userCreated) {
-  //       if (err){
-  //         console.log('Error: ', err);
-  //         return cb(err, null);
-  //       }
-  //       console.log('User created');
-  //       return cb(null, userCreated);
-  //     });
-  //   } else {
-  //     return cb(null, existingUser);
-  //   }
-  // });
 };
 
 module.exports = {
   createUserIfNotExistant: createUserIfNotExistant
 };
-
-
-// Endpoint.findOneAndUpdate({ 'username': username, 'route': route }, newData, {upsert: true},function(err, numberAffected, raw) {
-//   if (err) {
-//     return res.status(500).end();
-//   }
-//   res.status(201).end();
-// });
