45424df88e87d170cfb1f762027adb67d57d3542
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-02-24 15:30:25 -0800

    moment library working

--- src/server/api/endpointRouter/endpointRouterController.js
+++ src/server/api/endpointRouter/endpointRouterController.js
@@ -74,8 +74,9 @@ var postData = function(req, res, next) {
           //TODO --> we could do some data validation in here, checking for specific key-value pairs that the user passed through the UI
           //TODO --> Have to save to db the increment of the count
           newContent.id = endpoint.count;
-          newContent.createdAt = Moment.format('L');
-          newContent.updatedAt = Moment.format('L');
+          newContent.createdAt = Moment().format('L');
+          newContent.updatedAt = Moment().format('L');
+
           //for(var column in newContent) {
             //if(!endpoint.schemaDB[column]) {
               //console.log('before deleting', newContent);
@@ -88,7 +89,7 @@ var postData = function(req, res, next) {
             if (err) return res.status(500).json(err); 
             console.log(numAffected, rawResponse);
             //update count of objects in db
-            Endpoint.update({ 'username': username, 'route': route }, {$set: {'count': endpoint.count++}}, function(err, numAffected, rawResponse) {
+            Endpoint.update({ 'username': username, 'route': route }, {$set: {'count': ++endpoint.count}}, function(err, numAffected, rawResponse) {
               return res.status(201).end();
             });
           }); 
@@ -131,7 +132,7 @@ var changeData = function(req, res, next) {
           //    delete newContent[column];
           //  }
           //}
-          newContent.updatedAt = Moment.format('L');
+          newContent.updatedAt = Moment().format('L');
 
           var queryID = parseInt(req.query.id);
           var deleteQuery = {id: queryID};
