50618da83f5e6588f76a359e81f6a258529dfc68
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-02-24 16:55:51 -0800

    code cleanup

--- src/server/config/passport.js
+++ src/server/config/passport.js
@@ -23,12 +23,6 @@ module.exports = function(app) {
     });
   });
 
-  console.log('github client ID', config.github.clientID);
-
-  console.log('github client secret', config.github.clientSecret);
-
-  console.log('github callback', config.github.callback);
-
   // GitHub
   passport.use('github',
     new OAuth2Strategy({
