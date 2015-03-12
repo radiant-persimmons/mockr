24206a10ece2535f8ec565ab19e74e7120f1d417
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-03-12 15:29:51 -0700

    pr cleanup

--- src/server/api/auth/authController.js
+++ src/server/api/auth/authController.js
@@ -34,7 +34,6 @@ function authentication(req, res, next) {
 }
 
 function authenticationCallback(req, res, next) {
-  console.log('auth callback');
   return passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/home');
