cae4d8741d058968a3243863e477c92195419e12
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-03-23 11:07:55 +0000

    refactor to eliminate repeat code in endpointRouterController

--- src/server/api/endpointRouter/endpointRouterController.js
+++ src/server/api/endpointRouter/endpointRouterController.js
@@ -7,6 +7,7 @@ var utils = require('../../utils');
 var reportError = require('../../utils/errorReporter');
 
 module.exports = {
+  handler: handler,
   getData: getData,
   postData: postData,
   changeData: changeData,
@@ -15,11 +16,23 @@ module.exports = {
 
 ///////////
 
-function getData(req, res, next) {
+function handler(req, res, next) {
+  var actions = {
+    GET: getData,
+    POST: postData,
+    PUT: changeData,
+    DELETE: deleteData
+  };
+
   var username = req.params.username;
   var route = req.params[0];
   var method = req.method;
   var data;
+
+  // Check that method is supported
+  if (typeof actions[method] !== 'function') {
+    return reportError(new Error(method + 'method not supported'), next, 'Unsupported method for endpoints', 500);
+  }
 
   Endpoint.findOne({ 'username': username, 'route': route }, function (err, endpoint) {
     if (err || !endpoint) {
@@ -30,13 +43,8 @@ function getData(req, res, next) {
     utils.incrementCallCount(username, route, endpoint, method, function(err) {
       if (err) return reportError(err, next, 'Failed to get endpoint data', 500);
 
-      //if persistance is set to true, we let the user persist data, manipulate it and read it through their API endpoint
       if(endpoint.persistence === true) {
-        // apply queries passed through the endpoint url
-        data = utils.applyQueries(req, endpoint.data);
-
-        if (!data) return reportError(err, next, 'Failed to get endpoint data', 500);
-        return res.status(200).json(data);
+        actions[method](req, res, next, username, route, method, endpoint);
 
       //else, if persistance is set to false
       } else {
@@ -52,22 +60,19 @@ function getData(req, res, next) {
       }
     });
   });
+}
+
+function getData(req, res, next, username, route, method, endpoint) {
+
+        // apply queries passed through the endpoint url
+        var data = utils.applyQueries(req, endpoint.data);
+        if (!data) return reportError(err, next, 'Failed to get endpoint data', 500);
+        return res.status(200).json(data);
+
 };
 
-function postData(req, res, next) {
-  var username = req.params.username;
-  var route = req.params[0];
-  var method = req.method;
+function postData(req, res, next, username, route, method, endpoint) {
 
-  Endpoint.findOne({ 'username': username, 'route': route }, function (err, endpoint) {
-    if (err || !endpoint) {
-      return utils.findOneEndpointErrorHandler(err, username, route,
-                                               'Failed to create endpoint data');
-    } else {
-      utils.incrementCallCount(username, route, endpoint, method, function(err) {
-        if (err) return res.status(500).json(err);
-        //if persistance is set to true, we let the user persist data through their API endpoint
-        if (endpoint.persistence === true) {
           //run business logic
           logic.runLogic(endpoint.businessLogic, req, function(err, newContent) {
             if (err) return res.status(500).json(err);
@@ -86,34 +91,10 @@ function postData(req, res, next) {
               });
             });
           });
-        } else {
-          //if GET method data hasn't been set, end response
-          if (!endpoint.methods[method]) return res.status(500).end();
-
-          //get statusCode and data from user input
-          var statusCode = endpoint.methods[method].status;
-          var data = endpoint.methods[method].data;
-          return res.status(statusCode).json(data);
-        }
-      });
-    }
-  });
 };
 
-function changeData(req, res, next) {
-  var username = req.params.username;
-  var route = req.params[0];
-  var method = req.method;
+function changeData(req, res, next, username, route, method, endpoint) {
 
-  Endpoint.findOne({ 'username': username, 'route': route }, function (err, endpoint) {
-    if (err || !endpoint) {
-      return utils.findOneEndpointErrorHandler(err, username, route,
-                                               'Failed to update endpoint data');
-    } else {
-      utils.incrementCallCount(username, route, endpoint, method, function(err) {
-        if (err) return res.status(500).json(err);
-        //if persistance is set to true, we let the user persist data through their API endpoint
-        if (endpoint.persistence === true) {
           //we need a parameter passed to know what to change
           if (!req.query.id) {
             return res.status(500).end();
@@ -136,42 +117,17 @@ function changeData(req, res, next) {
               });
             });
           }
-        } else {
-          if (!endpoint.methods[method]) return res.status(500).end();
-
-          //get statusCode and data from user input
-          var statusCode = endpoint.methods[method].status;
-          var data = endpoint.methods[method].data;
-          return res.status(statusCode).json(data);
-        }
-      });
-    }
-  });
 };
 
-function deleteData(req, res, next) {
-  var username = req.params.username;
-  var route = req.params[0];
-  var method = req.method;
-  var data;
+function deleteData(req, res, next, username, route, method, endpoint) {
 
-  Endpoint.findOne({ 'username': username, 'route': route }, function (err, endpoint) {
-    if (err || !endpoint) {
-      return utils.findOneEndpointErrorHandler(err, username, route,
-                                               'Failed to get endpoint data');
-    } else {
-      utils.incrementCallCount(username, route, endpoint, method, function(err) {
-        if (err) return res.status(500).json(err);
-        //if persistance is set to true, we let the user persist data through their API endpoint
-        if (endpoint.persistence === true) {
           //we need a parameter passed to know what to change
           if (!req.query.id) {
             //if no id is passed in the url endpoint, end connection
             return res.status(500).end();
           } else {
-            data = endpoint.data;
             var queryID = parseInt(req.query.id);
-            var dataPoint = utils.lookForDataPoint(data, queryID);
+            var dataPoint = utils.lookForDataPoint(endpoint.data, queryID);
             if(!dataPoint) {
               return res.status(500).end();
             }
@@ -183,15 +139,4 @@ function deleteData(req, res, next) {
               return res.status(201).end();
             });
           }
-        } else {
-          if(!endpoint.methods[method]) return res.status(500).end();
-
-          //get statusCode and data from user input
-          var statusCode = endpoint.methods[method].status;
-          data = endpoint.methods[method].data;
-          return res.status(statusCode).json(data);
-        }
-      });
-    }
-  });
 };
--- src/server/api/endpointRouter/index.js
+++ src/server/api/endpointRouter/index.js
@@ -1,9 +1,9 @@
 var endpointRouterController = require('./endpointRouterController.js');
 
 module.exports = function(router) {
-  router.get('/1/:username/*', endpointRouterController.getData );
-  router.post('/1/:username/*', endpointRouterController.postData );
-  router.put('/1/:username/*', endpointRouterController.changeData);
-  router.delete('/1/:username/*', endpointRouterController.deleteData);
+  router.get('/1/:username/*', endpointRouterController.handler );
+  router.post('/1/:username/*', endpointRouterController.handler );
+  router.put('/1/:username/*', endpointRouterController.handler);
+  router.delete('/1/:username/*', endpointRouterController.handler);
 };
 
