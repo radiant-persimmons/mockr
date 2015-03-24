dee615bb4e4ed74f9edc293d37ab3f514eb35347
Andrew Smith <andrewsmith@alumni.stanford.edu>
2015-03-24 20:33:01 +0000

    write tests for #postData

--- src/server/api/endpointRouter/methodController.spec.js
+++ src/server/api/endpointRouter/methodController.spec.js
@@ -60,6 +60,7 @@ describe('UNIT: methodController', function() {
     };
     res = {
       status: sinon.stub().returns({
+        end: sinon.stub(),
         json: sinon.stub()
       })
     };
@@ -132,7 +133,101 @@ describe('UNIT: methodController', function() {
     });
   });
 
+  describe('#postData', function() {
+    var logic;
+    var newContent;
+    beforeEach(function() {
+      // fake data
+      endpoint.businessLogic = 'some logic';
+      newContent = {};
 
+      // load other modules needed
+      logic = require('../../utils/businessLogic.js');
+
+      // stub module methods
+      sinon.stub(logic, 'runLogic');
+      // error path
+      logic.runLogic.withArgs('error')
+        .yields('error', null);
+      // regular path
+      logic.runLogic.withArgs(sinon.match.string, sinon.match.object, sinon.match.func)
+        .yields(null, newContent);
+
+      sinon.stub(utils, 'augmentPostData');
+      utils.augmentPostData.returns(newContent);
+
+      sinon.stub(utils, 'insertPostDataToDb');
+      utils.insertPostDataToDb.withArgs('error').yields('error');
+      utils.insertPostDataToDb.withArgs(sinon.match.string, route, newContent, sinon.match.func)
+        .yields(null);
+
+      sinon.stub(utils, 'updateObjectCount');
+      utils.updateObjectCount.withArgs('error2').yields('error');
+      utils.updateObjectCount.withArgs(username, route, endpoint, sinon.match.func)
+        .yields(null);
+
+    });
+
+    afterEach(function() {
+      logic.runLogic.restore();
+      utils.augmentPostData.restore();
+      utils.insertPostDataToDb.restore();
+      utils.updateObjectCount.restore();
+    });
+
+    it('runs business logic', function() {
+      ctrl.postData(req, res, next, username, route, endpoint);
+      expect(logic.runLogic.calledWith(endpoint.businessLogic, req, sinon.match.func)).to.be.true;
+    });
+
+    it('handles error from business logic', function() {
+      // trigger error
+      endpoint.businessLogic = 'error';
+      ctrl.postData(req, res, next, username, route, endpoint);
+      expect(reportErrorStub.called).to.be.true;
+    });
+
+    it('returns 500 and newContent.res if present', function() {
+      newContent.res = 'present';
+      ctrl.postData(req, res, next, username, route, endpoint);
+      expect(res.status.calledWith(500)).to.be.true;
+      expect(res.status().json.calledWith(newContent.res)).to.be.true;
+    });
+
+    it('augments newContent', function() {
+      ctrl.postData(req, res, next, username, route, endpoint);
+      expect(utils.augmentPostData.calledWith(newContent, endpoint)).to.be.true;
+    });
+
+    describe('inserts post into database', function() {
+      it('handles an error in return', function() {
+        username = 'error';
+        ctrl.postData(req, res, next, username, route, endpoint);
+        expect(reportErrorStub.called).to.be.true;
+      });
+
+      describe('updates object count', function() {
+        it('calls utils.updateObjectCount with correct params', function() {
+          ctrl.postData(req, res, next, username, route, endpoint);
+          expect(utils.updateObjectCount.calledWith(username, route, endpoint, sinon.match.func)).to.be.true;
+        });
+
+        it('handles an error in return', function() {
+          username = 'error2';
+          ctrl.postData(req, res, next, username, route, endpoint);
+          expect(reportErrorStub.called).to.be.true;
+        });
+
+        it('returns 201 on completion', function() {
+          ctrl.postData(req, res, next, username, route, endpoint);
+          expect(res.status.calledWith(201)).to.be.true;
+          expect(res.status().end.called).to.be.true;
+        });
+      });
+    });
+
+
+  });
 });
 
 
